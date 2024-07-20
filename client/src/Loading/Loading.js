9efc0ad654e56ed56c9c561bf830e227cb6d139d
Chandan <94438598+Chandan157@users.noreply.github.com>
2024-07-20 16:47:36 +0530

    Loading added

---
+++ client/src/Loading/Loading.js
@@ -0,0 +1,15 @@
+// Loading.js
+
+import React from 'react';
+import './Loading.css'
+
+const Loading = () => {
+  return (
+    <div className="loading-screen">
+      <p>Loading...</p>
+      {/* You can customize the loading screen as needed */}
+    </div>
+  );
+};
+
+export default Loading;
